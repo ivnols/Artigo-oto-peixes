1d57765b277b36578b7091282ccb44674c09285d
Ivan Oliveira <oliivgomac@gmail.com>
2017-08-31 18:23:01 -0300

    Add files via upload

--- pca biplots.R
+++ pca biplots.R
@@ -1,23 +1,24 @@
-#Script bestinha pra calcular e plotar as PCA dos índices morfoecológicos dos otólitos
+library(ggplot2)
+library(ggfortify)
+mat.guild=read.csv("mat_guild.csv",sep=",",row.names=1)
 
-autoplot(prcomp(pca.mat.reg), data = mat.reg, colour = 'Region', loadings=T)
+pca.mat.guild=mat.guild[c(2,3,4,5)]
+
+autoplot(prcomp(pca.mat.guild), data = mat.guild, colour = 'Guild', loadings=T)
 
 autoplot(prcomp(pca.mat.guild, scale=T),data=mat.guild,colour='Guild',
-         loadings=T,loadings.label=T,loadings.label.size=2.5,
-         loadings.colour='blue',frame=T)
+         loadings=T,loadings.label=T,loadings.label.size=3,
+         loadings.colour='blue',loadings.label.colour='black',frame=T)
 
-mat.reg=read.csv("mat_reg.csv",sep=";",row.names=1)
+mat.reg=read.csv("mat_reg.csv",sep=",",row.names=1)
 
 pca.mat.reg=mat.reg[c(2,3,4,5)]
 
 autoplot(prcomp(pca.mat.reg,scale=T),data=mat.reg,colour='Region',
-         loadings=T,loadings.label=T,loadings.label.size=3.5,
-         loadings.label.colour='black',loadings.colour='black',frame=T)
+         loadings=T,loadings.label=T,loadings.label.size=3,
+         loadings.colour='blue',loadings.label.colour='black',frame=T)
+geom_point(aes(shape=factor(pca.mat.reg)))
 
-mat.guild=read.csv("mat_guild.csv",sep=";",row.names=1)
+autoplot(prcomp(pca.mat.reg,scale=T,data=mat.reg,frame=T))+
+  geom_point(aes(shape=mat.reg$Region,colour=mat.reg$Region),size=3)
 
-pca.mat.guild=mat.guild[c(2,3,4,5)]
-
-autoplot(prcomp(pca.mat.reg,scale=T),data=mat.guild,colour='Guild',
-         loadings=T,loadings.label=T,loadings.label.size=3.5,
-         loadings.label.colour='black',loadings.colour='blue',frame=T)
